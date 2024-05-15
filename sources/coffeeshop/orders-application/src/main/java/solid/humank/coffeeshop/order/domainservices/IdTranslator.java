754632005d21fb65edf43acce9f66bd13757c6c3
Kim Kao <yikaikao@gmail.com>
2024-05-15 23:27:15 +0800

    fix the stringbuffer issue through amazonq support.

--- sources/coffeeshop/orders-application/src/main/java/solid/humank/coffeeshop/order/domainservices/IdTranslator.java
+++ sources/coffeeshop/orders-application/src/main/java/solid/humank/coffeeshop/order/domainservices/IdTranslator.java
@@ -11,10 +11,12 @@ public class IdTranslator implements ITranslator<OrderId, String> {
     public OrderId translate(String transRequest) {
 
         String[] idString = transRequest.split("-");
-        StringBuilder sb = new StringBuilder(idString[1]);
+        String datetimePart = idString[1];
+        String seqNumPart = idString[2];
 
-        sb.insert(4, "/");
-        sb.insert(7, "/");
-        return new OrderId(Long.parseLong(idString[2]), OffsetDateTime.parse(idString[1]));
+        String formattedDatetime = datetimePart.substring(0, 4) + "/" + 
+            datetimePart.substring(4, 6) + "/" + datetimePart.substring(6); 
+        
+        return new OrderId(Long.parseLong(seqNumPart), OffsetDateTime.parse(formattedDatetime));
     }
 }
